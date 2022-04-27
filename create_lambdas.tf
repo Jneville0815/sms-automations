cf5d9a82284bd40d87bef7762d73f840b33ed0b1
Jimmy Neville <jimmyeneville@gmail.com>
2022-04-26 21:26:22 -0400

    Only run lambda once per month

--- create_lambdas.tf
+++ create_lambdas.tf
@@ -4,7 +4,7 @@ module "create_lambdas" {
   function_name         = each.key
   function_description  = each.value
   function_iam_role_arn = aws_iam_role.automation_role.arn
-  function_cron_name    = aws_cloudwatch_event_rule.every-hour.name
-  function_cron_arn     = aws_cloudwatch_event_rule.every-hour.arn
+  function_cron_name    = aws_cloudwatch_event_rule.first-monday-of-month-9am.name
+  function_cron_arn     = aws_cloudwatch_event_rule.first-monday-of-month-9am.arn
   global                = local.global
 }
